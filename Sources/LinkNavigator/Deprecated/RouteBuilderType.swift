94b568b3c5d76b7682babe0f41760195d96f17a2
stepan-bezhuk-axon <sbezhuk@axon.dev>
2025-04-27 22:47:44 +0300

    fix: added Sendable type

--- Sources/LinkNavigator/Deprecated/RouteBuilderType.swift
+++ Sources/LinkNavigator/Deprecated/RouteBuilderType.swift
@@ -7,5 +7,5 @@ public typealias MatchingViewController = MatchPathUsable & UIViewController
 public protocol RouteBuilder {
   var matchPath: String { get }
 
-  var build: (LinkNavigatorType, [String: String], DependencyType) -> MatchingViewController? { get }
+  var build: @Sendable (LinkNavigatorType, [String: String], DependencyType) -> MatchingViewController? { get }
 }
